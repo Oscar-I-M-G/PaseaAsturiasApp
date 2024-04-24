aff9f31e082be782059a03abc82b1f489c0ff018
Oscar Ivan <oscar.moreno4275@alumnos.udg.mx>
2024-04-24 19:00:24 +0200

    Good commit to push

--- app/src/main/java/com/oimg/paseaasturias/data/RepositoryImpl.kt
+++ app/src/main/java/com/oimg/paseaasturias/data/RepositoryImpl.kt
@@ -6,6 +6,8 @@ import com.oimg.paseaasturias.domain.Repository
 import com.oimg.paseaasturias.domain.model.SelectionModel
 import javax.inject.Inject
 
+
+// Extiende a Repository de nuestr capa Domain
 class RepositoryImpl @Inject constructor(private val apiService: PaseaAsturiasAPIService) :
     Repository {
 
@@ -14,7 +16,7 @@ class RepositoryImpl @Inject constructor(private val apiService: PaseaAsturiasAP
         return try {
             val response = apiService.getMyAPI()
             Log.i("API_SERVICE", "It works")
-            response?.map { it.toDomain() } ?: emptyList()
+            response.map { it.toDomain() }
         } catch (e: Exception) {
             Log.e("API_SERVICE", "An error occurred: ${e.message}")
             null
